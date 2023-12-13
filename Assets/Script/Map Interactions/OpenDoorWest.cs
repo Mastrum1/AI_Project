8d1d7a7b4a0ddeb1e47a4a5cde0bce563fd66760
ulysst <ulysstrachet@gmail.com>
2023-12-13 13:50:39 +0100

    Update Lights / Map / Added Player

---
+++ Assets/Script/Map Interactions/OpenDoorWest.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorWest : MonoBehaviour
+{
+    [SerializeField] GameObject _door;
+    [SerializeField] float translateSpeed = 1f;
+    void Start()
+    {
+        Vector3 mouvement = new Vector3(0,1f,0) * translateSpeed * Time.deltaTime;
+    }
+
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider colision)
+    {
+        if (colision.tag == "Player")
+        {
+            //ChangeTargetObjectPosition();
+            StartCoroutine(IncreaseRotation());
+        }
+    }
+
+    void ChangeTargetObjectPosition()
+    {
+        if (_door != null)
+        {
+            if (_door.transform.rotation.y < 105)
+            {
+                Quaternion newPosition = _door.transform.rotation;
+                newPosition.y = 0.1f;
+                _door.transform.rotation = newPosition;
+            }
+        }
+    }
+
+    private IEnumerator IncreaseRotation() 
+    {
+        float targetRotY = _door.transform.rotation.eulerAngles.y + 105f;
+        float rotationSpeed = 50f;
+        while (_door.transform.rotation.eulerAngles.y < targetRotY)
+        {
+            float newYRotation = _door.transform.rotation.eulerAngles.y + rotationSpeed * Time.deltaTime;
+            _door.transform.rotation = Quaternion.Euler(0f, newYRotation, 0f);
+
+            yield return null;
+        }
+        gameObject.gameObject.SetActive(false);
+       
+    }
+}

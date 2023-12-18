3d453ab8d44ecce41b3e73699147cd385308dddc
Mastrum1 <92383309+Mastrum1@users.noreply.github.com>
2023-12-18 17:24:25 +0100

    merge

--- Assets/Scripts/Behaviors/Attack.cs
+++ Assets/Scripts/Behaviors/Attack.cs
@@ -2,21 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.iOS;
 
 public class Attack : StateMachineBehaviour
 {
-    GameObject user;
+    private GameObject user;
+    private Player _playerManager;
+    private bool _isAttacking = false;
+
+    [SerializeField] private float _attackDelay;
+    [SerializeField] private float _damage;
+    
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        _playerManager = GameObject.Find("Player").GetComponent<Player>();
         user = animator.gameObject;
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // add damage amount to player
+        if (_isAttacking == false)
+        {
+            _isAttacking = true;
+            StartCoroutine(AttackDelay());
+            _playerManager.hp -= _damage;
+        }
     }
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //
+    }
+
+    IEnumerator AttackDelay()
+    {
+        yield return new WaitForSeconds(1f);
     }
 }

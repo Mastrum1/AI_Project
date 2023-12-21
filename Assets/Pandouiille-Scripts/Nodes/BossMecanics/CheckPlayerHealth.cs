ddc90f0a8e2abce4a3f6654d04f4c6531838e645
Alexandre Gouveia <92433601+Pandouiille@users.noreply.github.com>
2023-12-21 18:09:31 +0100

    Update CheckPlayerHealth.cs

--- Assets/Pandouiille-Scripts/Nodes/BossMecanics/CheckPlayerHealth.cs
+++ Assets/Pandouiille-Scripts/Nodes/BossMecanics/CheckPlayerHealth.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPlayerHealth : MonoBehaviour
+public class CheckPlayerHealth : DecoratorNode
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    private float currentHealth;
+    public float healthPercentage = 0.5f;
+
+    protected override void OnStart()
     {
-        
+        //float currentHealth = player.GetComponent<PlayerHealth>().currentHealth;
+        //float maxHealth = player.GetComponent<PlayerHealth>().maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override State OnUpdate()
     {
-        
+        if (!player)
+        {
+            return State.Failure;
+        }
+
+
+        if (currentHealth > healthPercentage)
+        {
+            return State.Success;
+        }
+        else
+        {
+            return State.Failure;
+        }
+    }
+
+    protected override void OnStop()
+    {
     }
 }
